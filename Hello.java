437fcc96c57d02eb98aa8094b8cf3cad0a41ccec
2795shital <shitalshelke58@gmail.com>
2024-07-31 09:03:32 -0700

    Fix bug-1

--- Hello.java
+++ Hello.java
@@ -3,5 +3,7 @@ public class Hello{
     {
         System.out.println("Hello World!");
         System.out.println("Welcome to world....");
+        int sum = 20 +90;
+        System.out.println("Sum = "+sum);
     }
 }
